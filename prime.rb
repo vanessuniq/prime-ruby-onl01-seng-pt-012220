7a1c3d2b3315f3486ce215b081297319d69c02e4
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-01 20:17:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(i) 
+  if i > 0 
+    possible_factors = (2..i**0.5).to_a 
+    test = possible_factors.any? {|factor| i % factor == 0}
+  else 
+    possible_factors = (-2..-(i**0.5)).to_a + (2..i**0.5).to_a
+    test = possible_factors.any? {|factor| i % factor == 0}
+  end 
+  !test 
+end 
