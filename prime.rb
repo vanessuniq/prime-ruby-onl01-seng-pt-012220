e5ecbdcb29bf6c57a3a8af18117c8407d469eb0c
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-01 20:22:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,8 @@ def prime?(i)
   if i > 0 
     possible_factors = (2..i**0.5).to_a 
     test = possible_factors.any? {|factor| i % factor == 0}
+    !test 
   else 
-    possible_factors = (-2..-(i**0.5)).to_a + (2..i**0.5).to_a
-    test = possible_factors.any? {|factor| i % factor == 0}
+    false 
   end 
-  !test 
 end 
