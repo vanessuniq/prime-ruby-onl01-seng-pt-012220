1a681fc17b46c4c06b7bf8b0a1b766786ed30d0c
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-01 20:23:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(i) 
+  if i > 1 
+    possible_factors = (2..i**0.5).to_a 
+    test = possible_factors.any? {|factor| i % factor == 0}
+    !test 
+  else 
+    false 
+  end 
+end 
